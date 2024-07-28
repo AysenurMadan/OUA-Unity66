5a2ce677d15a4e702a78200096fb25d6e19fc25d
Büşra ACAR <114398247+LadyAnyone@users.noreply.github.com>
2024-07-28 22:51:51 +0300

    add

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    
+    private AudioSource audioSource;
+    [SerializeField] AudioClip jump;
+    [SerializeField] AudioClip deathByFall;
+    [SerializeField] AudioClip pickingSound;
+
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    
+    public void Jump() {
+        audioSource.PlayOneShot(jump);
+    }
+
+    public void Jump()
+    {
+        audioSource.PlayOneShot(deathByFall);
+    }
+
+    public void Jump()
+    {
+        audioSource.PlayOneShot(pickingSound);
+    }
+
+
 }
